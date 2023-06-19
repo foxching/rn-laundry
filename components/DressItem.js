bba4bb943cbc70f1756f1580ee6d99ec498a637a
foxching <rechielagria@gmail.com>
2023-06-19 22:21:12 +0800

    finish home screen ui

--- components/DressItem.js
+++ components/DressItem.js
@@ -16,14 +16,42 @@ const DressItem = ({ item }) => {
         }}
       >
         <View>
-          {" "}
           <Image
             style={{ width: 70, height: 70 }}
             source={{ uri: item.image }}
           />
         </View>
-       
-       
+
+        <View>
+          <Text
+            style={{
+              width: 83,
+              fontSize: 17,
+              fontWeight: "500",
+              marginBottom: 7,
+            }}
+          >
+            {item.name}
+          </Text>
+          <Text style={{width:60, fontSize:15, color:"gray"}}>{item.price}</Text>
+        </View>
+
+        <Pressable style={{ width: 80 }}>
+          <Text
+            style={{
+              borderColor: "gray",
+              borderWidth: 0.8,
+              borderRadius: 4,
+              marginVertical: 10,
+              textAlign: "center",
+              padding: 5,
+              color: "#088F8F",
+              fontWeight: "bold",
+            }}
+          >
+            Add
+          </Text>
+        </Pressable>
       </Pressable>
     </View>
   );
